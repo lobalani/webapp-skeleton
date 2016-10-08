759b1da8b357ccf72e62273e3c9cd1d4bcd463a0
Lokesh Balani <lokeshbalani1@outlook.com>
2016-10-08 18:44:48 +0530

    Gulpfile.js : Enclosed code in  an IIFE.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,75 +1,79 @@
-//require dependencies
-var gulp = require('gulp');
-var semver = require('semver');
-var $ = require('gulp-load-plugins')({ lazy: true });
+(function(){
+    "use strict";
 
-//require config files
-var packageJson = require('./package.json');
+    //require dependencies
+    var gulp = require('gulp');
+    var semver = require('semver');
+    var $ = require('gulp-load-plugins')({ lazy: true });
 
-//constants
-var EXPECTED_NODE_VERSION = packageJson.engines.node;
+    //require config files
+    var packageJson = require('./package.json');
 
-gulp.task('default', ["nodeVersion:check"], function() {
-    log("\n\nBUILD SUCCESSFUL");
-});
+    //constants
+    var EXPECTED_NODE_VERSION = packageJson.engines.node;
 
-/**
- * Checking Node version installed on the system.
- * Need to have Node.js v5.6.0 for the setup to work correctly
- * 
- **/
-gulp.task('nodeVersion:check', function() {
-    log("Checking Node version...");
+    gulp.task('default', ["nodeVersion:check"], function() {
+        log("\n\nBUILD SUCCESSFUL");
+    });
 
-    var actualVersion = process.version;
+    /**
+     * Checking Node version installed on the system.
+     * Need to have Node.js v5.6.0 for the setup to work correctly
+     * 
+     **/
+    gulp.task('nodeVersion:check', function() {
+        log("Checking Node version...");
 
-    if(semver.neq(EXPECTED_NODE_VERSION, actualVersion)) {
-        errorLog("Incorrect Node Version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
-    } else {
-        log("Correct Node Version Installed");
-    }
-});
+        var actualVersion = process.version;
 
-
-/* Helper Functions */
-
-/**
- * Log output message in inverse color
- */
-function log(msg) {
-    if(typeof(msg) === "object"){
-        for(var key in msg){
-            if(msg.hasOwnProperty(item)){
-                $.util.log($.util.colors.inverse(msg[key]));
-            }
+        if(semver.neq(EXPECTED_NODE_VERSION, actualVersion)) {
+            errorLog("Incorrect Node Version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+        } else {
+            log("Correct Node Version Installed");
         }
-    } else {
-        $.util.log($.util.colors.inverse(msg));
-    }
-};
+    });
 
-/**
- * Log error messages in red color
- */
-function error(msg) {
-    if(typeof(msg) === "object"){
-        for(var key in msg){
-            if(msg.hasOwnProperty(item)){
-                $.util.log($.util.colors.red(msg[key]));
+
+    /* Helper Functions */
+
+    /**
+     * Log output message in inverse color
+     */
+    function log(msg) {
+        if(typeof(msg) === "object"){
+            for(var key in msg){
+                if(msg.hasOwnProperty(item)){
+                    $.util.log($.util.colors.inverse(msg[key]));
+                }
             }
+        } else {
+            $.util.log($.util.colors.inverse(msg));
         }
-    } else {
-        $.util.log($.util.colors.red(msg));
-    }
-};
+    };
 
-/**
- * Log error messages using error() helper function and exit the process
- */
-function errorLog(errorMsg) {
-    error("*** Start of Error ***");
-    error(errorMsg);
-    error("*** End of Error ***");
+    /**
+     * Log error messages in red color
+     */
+    function error(msg) {
+        if(typeof(msg) === "object"){
+            for(var key in msg){
+                if(msg.hasOwnProperty(item)){
+                    $.util.log($.util.colors.red(msg[key]));
+                }
+            }
+        } else {
+            $.util.log($.util.colors.red(msg));
+        }
+    };
 
-    process.exit(-1);
-};
+    /**
+     * Log error messages using error() helper function and exit the process
+     */
+    function errorLog(errorMsg) {
+        error("*** Start of Error ***");
+        error(errorMsg);
+        error("*** End of Error ***");
+
+        process.exit(-1);
+    };
+})();
