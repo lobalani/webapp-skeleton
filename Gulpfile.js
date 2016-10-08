ae33a46d7b159a4186404ded27511e8434222013
Lokesh Balani <lokeshbalani1@outlook.com>
2016-10-08 18:55:17 +0530

    Gulpfile.js : Refactored to remove jshint errors

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,5 @@
 (function(){
-    "use strict";
+    'use strict';
 
     //require dependencies
     var gulp = require('gulp');
@@ -12,8 +12,51 @@
     //constants
     var EXPECTED_NODE_VERSION = packageJson.engines.node;
 
-    gulp.task('default', ["nodeVersion:check"], function() {
-        log("\n\nBUILD SUCCESSFUL");
+    /* Helper Functions */
+
+    /**
+     * Log output message in inverse color
+     */
+    function log(msg) {
+        if(typeof(msg) === 'object'){
+            for(var key in msg){
+                if(msg.hasOwnProperty(key)){
+                    $.util.log($.util.colors.inverse(msg[key]));
+                }
+            }
+        } else {
+            $.util.log($.util.colors.inverse(msg));
+        }
+    }
+
+    /**
+     * Log error messages in red color
+     */
+    function error(msg) {
+        if(typeof(msg) === 'object'){
+            for(var key in msg){
+                if(msg.hasOwnProperty(key)){
+                    $.util.log($.util.colors.red(msg[key]));
+                }
+            }
+        } else {
+            $.util.log($.util.colors.red(msg));
+        }
+    }
+
+    /**
+     * Log error messages using error() helper function and exit the process
+     */
+    function errorLog(errorMsg) {
+        error('*** Start of Error ***');
+        error(errorMsg);
+        error('*** End of Error ***');
+
+        process.exit(-1);
+    }
+
+    gulp.task('default', ['nodeVersion:check'], function() {
+        log('\n\nBUILD SUCCESSFUL');
     });
 
     /**
@@ -22,58 +65,14 @@
      * 
      **/
     gulp.task('nodeVersion:check', function() {
-        log("Checking Node version...");
+        log('Checking Node version...');
 
         var actualVersion = process.version;
 
         if(semver.neq(EXPECTED_NODE_VERSION, actualVersion)) {
-            errorLog("Incorrect Node Version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+            errorLog('Incorrect Node Version: expected ' + EXPECTED_NODE_VERSION + ', but was ' + actualVersion);
         } else {
-            log("Correct Node Version Installed");
+            log('Correct Node Version Installed');
         }
     });
-
-
-    /* Helper Functions */
-
-    /**
-     * Log output message in inverse color
-     */
-    function log(msg) {
-        if(typeof(msg) === "object"){
-            for(var key in msg){
-                if(msg.hasOwnProperty(item)){
-                    $.util.log($.util.colors.inverse(msg[key]));
-                }
-            }
-        } else {
-            $.util.log($.util.colors.inverse(msg));
-        }
-    };
-
-    /**
-     * Log error messages in red color
-     */
-    function error(msg) {
-        if(typeof(msg) === "object"){
-            for(var key in msg){
-                if(msg.hasOwnProperty(item)){
-                    $.util.log($.util.colors.red(msg[key]));
-                }
-            }
-        } else {
-            $.util.log($.util.colors.red(msg));
-        }
-    };
-
-    /**
-     * Log error messages using error() helper function and exit the process
-     */
-    function errorLog(errorMsg) {
-        error("*** Start of Error ***");
-        error(errorMsg);
-        error("*** End of Error ***");
-
-        process.exit(-1);
-    };
 })();
